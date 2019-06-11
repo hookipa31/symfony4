cb2eb7385caeb8d4610084c5c52d48ede047a34a
Yvon Huynh <yvon@agence-hookipa.com>
2019-06-11 12:47:54 +0200

    add entity and controller

---
+++ src/Entity/Task.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: poste5hookipa
+ * Date: 2019-06-11
+ * Time: 12:25
+ */
+namespace App\Entity;
+
+class Task
+{
+    protected $task;
+    protected $dueDate;
+
+    public function getTask()
+    {
+        return $this->task;
+    }
+
+    public function setTask($task)
+    {
+        $this->task = $task;
+    }
+
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(\DateTime $dueDate = null)
+    {
+        $this->dueDate = $dueDate;
+    }
+}
