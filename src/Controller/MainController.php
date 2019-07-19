4a3dc253c00de98daa7280a80460181ddb3ba87c
Yvon Huynh <yvon@agence-hookipa.com>
2019-07-19 17:16:30 +0200

    added example of repository

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -16,11 +16,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\UserRepository;
+use App\Entity\User;
 
 class MainController extends AbstractController
 {
+    /**
+     * @Route("/home", name="app_home")
+     */
     public function index()
     {
+        //do some tests
+      /* $em =  $this->getDoctrine()->getManager();
+
+$query = $em->createQuery('select u from \App\Entity\User');
+$user = $query->getResult();
+*/
+
+      $repo = $this->getDoctrine()->getRepository(User::class);
+      $user =  $repo->findAllUsers();
+
         return $this->render(
             'home.html.twig',
             ['message' => 'Vous êtes dans la Homepage']
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,6 +19,12 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
+    public function findAllUsers():array
+    {
+       $qb = $this->createQueryBuilder('u')
+       ->getQuery();
+       return $qb->execute();
+}
     // /**
     //  * @return User[] Returns an array of User objects
     //  */
